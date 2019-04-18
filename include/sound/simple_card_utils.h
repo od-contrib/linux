e27d2810b09c769c0b0ae5784095e657e5e0ce04
Paul Cercueil <paul@crapouillou.net>
2019-04-18 03:23:15 +0200

    ASoC: simple-card-utils: add asoc_simple_card_of_parse_pin_switches()
    This function is a helper that permits to create pin switch controls for
    a list of widgets whose names are listed in the PREFIX "pin-switches"
    devicetree property.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- include/sound/simple_card_utils.h
+++ include/sound/simple_card_utils.h
@@ -124,6 +124,8 @@ int asoc_simple_card_of_parse_routing(struct snd_soc_card *card,
 				      char *prefix);
 int asoc_simple_card_of_parse_widgets(struct snd_soc_card *card,
 				      char *prefix);
+int asoc_simple_card_of_parse_pin_switches(struct snd_soc_card *card,
+					   char *prefix);
 
 int asoc_simple_card_init_jack(struct snd_soc_card *card,
 			       struct asoc_simple_jack *sjack,
