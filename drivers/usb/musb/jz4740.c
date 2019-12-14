e21a686d784ac1ab4e7bc623928172878f6d2beb
Paul Cercueil <paul@crapouillou.net>
2019-12-14 22:43:34 +0100

    usb: musb: jz4740: Suppress useless field in priv structure
    The 'dev' field was never read anywhere.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- drivers/usb/musb/jz4740.c
+++ drivers/usb/musb/jz4740.c
@@ -17,7 +17,6 @@
 #include "musb_core.h"
 
 struct jz4740_glue {
-	struct device           *dev;
 	struct platform_device  *musb;
 	struct clk		*clk;
 };
@@ -149,7 +148,6 @@ static int jz4740_probe(struct platform_device *pdev)
 	musb->dev.dma_mask		= &musb->dev.coherent_dma_mask;
 	musb->dev.coherent_dma_mask	= DMA_BIT_MASK(32);
 
-	glue->dev			= &pdev->dev;
 	glue->musb			= musb;
 	glue->clk			= clk;
 
