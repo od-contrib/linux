05df80e3e819b0c49d403d306362696569367b74
Paul Cercueil <paul@crapouillou.net>
2021-01-17 10:27:45 +0000

    drm: bridge/panel: Cleanup connector on bridge detach
    If we don't call drm_connector_cleanup() manually in
    panel_bridge_detach(), the connector will be cleaned up with the other
    DRM objects in the call to drm_mode_config_cleanup(). However, since our
    drm_connector is devm-allocated, by the time drm_mode_config_cleanup()
    will be called, our connector will be long gone. Therefore, the
    connector must be cleaned up when the bridge is detached to avoid
    use-after-free conditions.
    
    Fixes: 13dfc0540a57 ("drm/bridge: Refactor out the panel wrapper from the lvds-encoder bridge.")
    Cc: <stable@vger.kernel.org> # 4.12+
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- drivers/gpu/drm/bridge/panel.c
+++ drivers/gpu/drm/bridge/panel.c
@@ -87,6 +87,10 @@ static int panel_bridge_attach(struct drm_bridge *bridge,
 
 static void panel_bridge_detach(struct drm_bridge *bridge)
 {
+	struct panel_bridge *panel_bridge = drm_bridge_to_panel_bridge(bridge);
+	struct drm_connector *connector = &panel_bridge->connector;
+
+	drm_connector_cleanup(connector);
 }
 
 static void panel_bridge_pre_enable(struct drm_bridge *bridge)
