0659e753488569e7b357bae4feff26721028393e
Paul Cercueil <paul@crapouillou.net>
2021-06-14 10:55:37 +0100

    iio: core: Forbid use of both labels and extended names
    Extended names are a problem for user-space as they make the filenames
    in sysfs sometimes not parsable. They are now deprecated in favor of
    labels.
    
    This change makes sure that a device driver won't provide both labels
    and extended names for its channels. It has never been the case and we
    don't want it to happen.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>
    Reviewed-by: Alexandru Ardelean <ardeleanalex@gmail.com>

--- drivers/iio/industrialio-core.c
+++ drivers/iio/industrialio-core.c
@@ -1836,6 +1836,24 @@ static int iio_check_unique_scan_index(struct iio_dev *indio_dev)
 	return 0;
 }
 
+static int iio_check_extended_name(const struct iio_dev *indio_dev)
+{
+	unsigned int i;
+
+	if (!indio_dev->info->read_label)
+		return 0;
+
+	for (i = 0; i < indio_dev->num_channels; i++) {
+		if (indio_dev->channels[i].extend_name) {
+			dev_err(&indio_dev->dev,
+				"Cannot use labels and extend_name at the same time\n");
+			return -EINVAL;
+		}
+	}
+
+	return 0;
+}
+
 static const struct iio_buffer_setup_ops noop_ring_setup_ops;
 
 int __iio_device_register(struct iio_dev *indio_dev, struct module *this_mod)
@@ -1857,6 +1875,10 @@ int __iio_device_register(struct iio_dev *indio_dev, struct module *this_mod)
 		indio_dev->label = label;
 
 	ret = iio_check_unique_scan_index(indio_dev);
+	if (ret < 0)
+		return ret;
+
+	ret = iio_check_extended_name(indio_dev);
 	if (ret < 0)
 		return ret;
 
